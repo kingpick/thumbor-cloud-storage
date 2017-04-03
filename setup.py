05d8ac38df076c04cbb4e9f42c1c05938d1cc4ff
fabito <fuechi@ciandt.com>
2017-04-03 13:50:24 -0300

    adjust deps versions

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import setup
 from setuptools import find_packages
 
 REQUIREMENTS = [
-  "thumbor==6.3.1",
-  "google-cloud-storage==1.0.0",
-  "google-auth==0.10.0"
+  "thumbor>=6.3.0",
+  "google-cloud-storage==1.0.0"
 ]
 
 setup(
